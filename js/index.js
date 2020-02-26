929545d878ea42b8d5578afced4da1bf4d720314
yusu <dbtnss@naver.com>
2020-02-26 11:36:14 +0900

    header

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
-var burgerMenuOn = document.querySelector(".burger_menu"),
-    bergerMenuOff = document.querySelector(".exit"),
-    mobileHeader = document.querySelector(".header_1depth_m");
+var burgerMenuOn = document.querySelector(".burger_menu"), // 버거메뉴 열기
+    bergerMenuOff = document.querySelector(".exit"), // 버거메뉴 닫기
+    mobileHeader = document.querySelector(".header_1depth_m"), // 버거메뉴
+    menu = document.querySelectorAll(".menu_m"), // 메뉴
+    detailMenu = document.querySelectorAll(".detail_menu_m");
 
 burgerMenuOn.addEventListener("click", function() {
     mobileHeader.classList.add("on");
-});
+}); // 버거메뉴 열기
 
 bergerMenuOff.addEventListener("click", function() {
     mobileHeader.classList.remove("on");
-});
+}); // 버거메뉴 닫기
+
+for (var i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", function(e) {
+        e.preventDefault();
+
+        if (e.target.nextSibling.nextSibling.classList[1] === "on") {
+            e.target.nextSibling.nextSibling.classList.remove("on");
+        } else {
+            for (var j = 0; j < menu.length; j++) {
+                detailMenu[j].classList.remove("on");
+            }
+
+            e.target.nextSibling.nextSibling.classList.add("on");
+        }
+    });
+}
+
+/* 버거메뉴 on/off */
