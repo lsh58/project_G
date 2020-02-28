1e9d9499746d4b3a5b101b3ba634e55bc7077da2
jhjang92 <60205464+jhjang92@users.noreply.github.com>
2020-02-28 10:51:11 +0900

    T
    T

--- js/sub01.js
+++ js/sub01.js
@@ -3,9 +3,8 @@ contentBox[0].style.height = document.getElementsByClassName('sub_con_on')[0].of
 
 function changeContent(objs){
     event.preventDefault();
-    // console.log(objs);
 
-    // subConOnClass -> sub_con_on 클래스가 있는애의 객체를 담은 변수
+    // subConOnClass -> sub_con_on 클래스가 있는 객체를 담은 변수
     var subConOnClass = document.getElementsByClassName('sub_con_on');
     //subCons -> onclick으로 함수호출시 클릭한 객체의 정보를 담은 변수
     var subCons = document.getElementsByClassName(objs.id);
@@ -16,12 +15,10 @@ function changeContent(objs){
 
         subCons[0].classList.add('sub_con_on');
         subCons[0].classList.remove('sub_con_off');
+
+        contentBox[0].style.height = subCons[0].offsetHeight + "px";
     }else{
         console.log("sub_con_on 을 갖고있는 객체와 클릭한 객체가 동일");
     }
-
-    contentBox[0].style.height = subCons[0].offsetHeight + "px";
-
-    // console.log(contentBox[0].style.height);
 
 }
